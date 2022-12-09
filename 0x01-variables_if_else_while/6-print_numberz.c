a8f747052932abfda8e3251ae267398c71558600
Omaranw <Omar.aglan91@gmail.com>
2022-12-09 06:16:09 -0800

    fix task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - main block of code 
+* main - main block of code
 * we have to print num from 0 - 10
 * with using only (putchar) - so as we
-* Have seen before using for loop - 
-* if digit is less than 10 - putchar it 
+* Have seen before using for loop -
+* if digit is less than 10 - putchar it
 * check agian - if >= 10 then end loop
 * Return: Always 0 (Success)
 */
