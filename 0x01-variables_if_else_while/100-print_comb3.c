5d30c63e75bb05415c0f11762186c775144cd890
Omaranw <Omar.aglan91@gmail.com>
2022-12-09 16:03:02 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * a program that prints all possible
+ */
+int main(void)
+{
+	int comb;
+	int desk = 0;
+
+	while (desk < 10)
+	{
+		comb = 0;
+		while (comb < 10)
+		{
+			if (desk != comb && desk < comb)
+			{
+				putchar('0' + desk);
+				putchar('0' + comb);
+
+				if (comb + desk != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			comb++;
+		}
+		desk++;
+	}
+	putchar('\n');
+	return (0);
+}
